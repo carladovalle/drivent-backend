fd032a16b344edb68e0f907d5c2533fad63fa456
Renato da Silva Viana <verkylen@gmail.com>
2023-03-02 17:22:00 -0300

    fix: sessions-factory's bug

--- tests/factories/sessions-factory.ts
+++ tests/factories/sessions-factory.ts
@@ -1,9 +1,9 @@
-import { Session } from "@prisma/client";
+import { Session, User } from "@prisma/client";
 import { createUser } from "./users-factory";
 import { prisma } from "@/config";
 
-export async function createSession(token: string): Promise<Session> {
-  const user = await createUser();
+export async function createSession(token: string, user?: User): Promise<Session> {
+  user = user || await createUser();
 
   return prisma.session.create({
     data: {

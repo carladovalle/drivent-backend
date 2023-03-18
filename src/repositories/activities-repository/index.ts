bddcaabff3e719e475ecc4c12b83596e9b6bd652
Dayane <dayanecarolina.oliveira@gmail.com>
2023-03-18 16:16:24 -0300

    feat: activities repository

--- src/repositories/activities-repository/index.ts
+++ src/repositories/activities-repository/index.ts
@@ -11,8 +11,74 @@ async function findDates() {
   });
 }
 
+async function findActivitiesWithLocals(date: Date) {
+  return prisma.local.findMany({
+    include: {
+      Activity: {
+        where: {
+          date,
+        },
+        orderBy: {
+          date: "asc",
+        },
+        include: {
+          _count: {
+            select: {
+              UserActivity: true,
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+async function findDaysWithActivities() {
+  return prisma.activity.groupBy({
+    by: ["date"],
+    orderBy: {
+      date: "asc",
+    },
+  });
+}
+
+async function getActivityById(activityId: number) {
+  return prisma.activity.findFirst({
+    where: {
+      id: activityId,
+    },
+  });
+}
+
+async function getAllActivitiesFromUser(userId: number) {
+  return prisma.userActivity.findMany({
+    where: {
+      userId,
+    },
+    include: {
+      Activity: true,
+    },
+  });
+}
+
+async function getUserActivity(userId: number) {
+  return prisma.userActivity.findMany({
+    where: {
+      userId,
+    },
+    select: {
+      activityId: true,
+    },
+  });
+}
+
 const activitiesRepository = {
   findDates,
+  findActivitiesWithLocals,
+  findDaysWithActivities,
+  getActivityById,
+  getAllActivitiesFromUser,
+  getUserActivity,
 };
 
 export default activitiesRepository;
